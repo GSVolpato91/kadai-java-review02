c91b2e270e55cfca9a7088efa13ab4b9c5309fa7
GSVolpato91 <guido.svolpato@gmail.com>
2023-11-13 21:11:39 +0900

    最初の課題の修正。

--- src/sample/Review01.java
+++ src/sample/Review01.java
@@ -3,18 +3,14 @@ package sample;
 public class Review01 {
 
     public static void main(String[] args) {
-        add (1500, 150);
-        double tax = add(1500, 0.10);
-        System.out.println("Consumption tax is" + tax + "yen.");
+        int price = 1800;
+        double tax = tax(price, 0.10);
+        System.out.println("The price including tax for a"+ price + "yen item is" + (price + tax )+ "yen." + "Consumption tax is" + tax + "yen.");
     }
-    public static void add(int num1, int num2) {
-        int result = num1 + num2;
-        System.out.println("The tax-inclusive price of the item is" + result + "yen.");
-    }
-        public static double add(double num1, double num2) {
+        public static double tax(double num1, double num2) {
             double result = num1 * num2;
             return result;
-      
+
     }
 
 }
