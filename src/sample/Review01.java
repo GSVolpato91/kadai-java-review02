45c4b106796b87e6f3cdda8ae72d79f02e67ddbb
GSVolpato91 <guido.svolpato@gmail.com>
2023-11-13 23:50:05 +0900

    修正(2回目)

--- src/sample/Review01.java
+++ src/sample/Review01.java
@@ -4,9 +4,13 @@ public class Review01 {
 
     public static void main(String[] args) {
         int price = 1800;
-        double tax = tax(price, 0.10);
-        System.out.println("The price including tax for a"+ price + "yen item is" + (price + tax )+ "yen." + "Consumption tax is" + tax + "yen.");
+        double tax = (price * 0.1);
+        short b = (short) tax;
+        int c = (int) tax;
+        System.out.println("The price including tax for a"+ price + "yen item is" + (price + c)+ "yen." + "Consumption tax is" + c + "yen.");
     }
+    
+    
         public static double tax(double num1, double num2) {
             double result = num1 * num2;
             return result;
