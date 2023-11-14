8ca1906f520618faa3dafab40cfdaf4740308ae2
GSVolpato91 <guido.svolpato@gmail.com>
2023-11-14 10:49:58 +0900

    修正(3回目)

--- src/sample/Review01.java
+++ src/sample/Review01.java
@@ -4,15 +4,13 @@ public class Review01 {
 
     public static void main(String[] args) {
         int price = 1800;
-        double tax = (price * 0.1);
-        short b = (short) tax;
-        int c = (int) tax;
+        double tax = (price * 0.10);
+       int c = (int) tax;
         System.out.println("The price including tax for a"+ price + "yen item is" + (price + c)+ "yen." + "Consumption tax is" + c + "yen.");
     }
     
-    
-        public static double tax(double num1, double num2) {
-            double result = num1 * num2;
+        public static double tax(double price, double tax) {
+            double result = price * tax;
             return result;
 
     }
