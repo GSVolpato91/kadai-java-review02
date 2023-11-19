d0cec646f0cb9b944b772294a85a99aa1077ba0c
GSVolpato91 <guido.svolpato@gmail.com>
2023-11-19 12:46:31 +0900

    FizｚBuzzの課題

--- src/sample/Review02.java
+++ src/sample/Review02.java
@@ -3,14 +3,19 @@ package sample;
 public class Review02 {
 
     public static void main(String[] args) {
-       for (int i = 1; i <= 100; i++) {
+        //Conditions
+        for (int i = 1; i <= 100; i++) {
+            //FizzBuzz divisibile by 3 AND 5
            if (i % 3 == 0 && i % 5 == 0) {
                System.out.println("FizzBuzz");
            } else if (i % 5 == 0) {
+               // Buzz divisible by 5 ONLY
                System.out.println("Buzz");
            } else if (i % 3== 0 ) {
+               // Fizz divisible by 3 ONLY
                    System.out.println("Fizz");
                } else {
+                   //NOT divisible by 3 AND 5
                    System.out.println(i);
            }
        }
