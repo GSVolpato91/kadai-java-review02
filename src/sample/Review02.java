f3ad5ea0f97a71f99343faa3e03562fda4086d5a
GSVolpato91 <guido.svolpato@gmail.com>
2023-11-19 12:40:32 +0900

    fizzbuzzの課題

--- src/sample/Review02.java
+++ src/sample/Review02.java
@@ -9,12 +9,10 @@ public class Review02 {
            } else if (i % 5 == 0) {
                System.out.println("Buzz");
            } else if (i % 3== 0 ) {
-                   System.out.println("Fizz");   
+                   System.out.println("Fizz");
                } else {
                    System.out.println(i);
            }
        }
-
     }
-
 }
